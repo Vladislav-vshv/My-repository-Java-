e9d3002626ce69379f61cf0a4d781691c3ffd671
Vladislav-vshv <neytronpal@gmail.com>
2024-08-06 19:49:05 +0400

    Песочница

---
+++ sandbox/src/main/java/gram.java
@@ -0,0 +1,6 @@
+public class gram {
+    public static void main(String[] args) {
+        // Показать в консоли сообщение Welcome to Java!
+        System.out.println("Welcome to Java!");
+    }
+}
